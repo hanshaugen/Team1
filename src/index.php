dd64e61d94e8e52b4f1281244db124d1623154b5
hanshaugen <97103592+hanshaugen@users.noreply.github.com>
2025-02-14 12:22:10 +0100

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting App</title>
+</head>
+<body>
+    <form action="greet.php" method="post">
+        <label for="name">Enter your name:</label>
+        <input type="text" id="name" name="name">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
